51eac968329b50776b1db1ea031beaec3a6f0924
8AREFIEV8 <7759191ai@gmail.com>
2023-10-25 20:15:48 +0300

    commit lesson1

--- 01-variables/homework/lesson/main.js
+++ 01-variables/homework/lesson/main.js
@@ -1,2 +1,50 @@
 let user = 'Igor';
 let age = 35;
+let boolean = 5 > 1;
+console.log(boolean);
+let num = 100;
+let string = 'I love programming ';
+console.log(string);
+let num1 = null;
+let num2;
+console.log(num2);
+let obj = {};
+console.log(obj);
+let fun = function() {
+  
+  'Hello world'
+}
+fun();
+console.log(fun);
+
+let res = 45 == 25;
+console.log(res);
+let res1 = '25' == '25';
+console.log(res1);
+let res2 = '25' === 28;
+let res3 = true === false;
+console.log(res3);
+
+let a = '10';
+let b = 10;
+let res4 = a == b;
+let res5 = a === b; 
+console.log(res4);
+
+let c = 12;
+let d = true;
+let res6 = c == d;
+console.log(res6);
+
+let x = 25;
+let x1 = null;
+console.log(x == x1);
+
+let xx;
+let zz = true;
+console.log(xx === zz);
+
+let box;
+let box1 = null;
+console.log(box === box1);
+
