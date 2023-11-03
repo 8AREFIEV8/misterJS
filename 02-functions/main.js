cb90d462b80e4e939cb2ad80b59f07c6041cbd63
8AREFIEV8 <7759191ai@gmail.com>
2023-11-03 23:00:37 +0300

    second commit

--- 02-functions/main.js
+++ 02-functions/main.js
@@ -13,4 +13,16 @@ if(user === 'Alex') {
   console.log('Вы еще не зарегистрировались. Зарегистрироваться?');
 }
 
+const health = 30;
 
+if (health >= 100) {
+  console.log('ВЫ здоровы');
+}else if(health >= 75) {
+  console.log('Не мешало бы подлечиться');
+}else if(health >= 50) {
+  console.log('Срочно надо подлечиться');
+}else if(health === 0) {
+  console.log('Вы мертвы');
+}else {
+  console.log(`Ваш уровень здоровья ${health}`);
+}
