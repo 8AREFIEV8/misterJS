3f452c75fcb50858e122a634654ddfc2c73bf50c
8AREFIEV8 <7759191ai@gmail.com>
2023-11-03 22:36:49 +0300

    commit1

---
+++ 01-variables/homework/lesson/02-functions/main.js
@@ -0,0 +1,16 @@
+const user = 'Igor';
+const user2 = 'Alex';
+
+if(user === 'Igor') {
+  console.log(`Приветствую тебя ${user}`);
+}else {
+  console.log('Вы еще не зарегистрировались. Зарегистрироваться?');
+}
+
+if(user === 'Alex') {
+  console.log(`Приветствую тебя ${user}`);
+}else {
+  console.log('Вы еще не зарегистрировались. Зарегистрироваться?');
+}
+
+
